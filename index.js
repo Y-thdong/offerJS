bd7df44e9b732287b23f2af8507b9acd2278974f
Y-thdong <tianhuidong2017@outlook.com>
2021-01-26 16:14:31 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// .----------------.  .----------------.  .----------------. 
+// | .--------------. || .--------------. || .--------------. |
+// | |  ____  ____  | || |  ________    | || |  _________   | |
+// | | |_   ||   _| | || | |_   ___ `.  | || | |  _   _  |  | |
+// | |   | |__| |   | || |   | |   `. \ | || | |_/ | | \_|  | |
+// | |   |  __  |   | || |   | |    | | | || |     | |      | |
+// | |  _| |  | |_  | || |  _| |___.' / | || |    _| |_     | |
+// | | |____||____| | || | |________.'  | || |   |_____|    | |
+// | |              | || |              | || |              | |
+// | '--------------' || '--------------' || '--------------' |
+//  '----------------'  '----------------'  '----------------' 
+
 // let board = [
 //         ["A", "B", "C", "E"],
 //         ["S", "F", "C", "S"],
@@ -5,7 +17,7 @@
 //     ],
 //     word = "ABCCED";
 
-// // var exist = function (board, word) {
+// // let exist = function (board, word) {
 // //     let pos = [];
 // //     for (let i = 0; i < board.length; i++) {
 // //         let temp = board[i].indexOf(word[0]);
@@ -24,25 +36,25 @@
 // // };
 // // console.log(exist(board, word));
 
-// var exist = function (board, word) {
-//     var row = board.length;
+// let exist = function (board, word) {
+//     let row = board.length;
 //     // 默认是二维矩阵
-//     var col = board[0].length;
+//     let col = board[0].length;
 
-//     var dfs = function (i, j, board, word, index) {
+//     let dfs = function (i, j, board, word, index) {
 //         if (i < 0 || i >= row || j < 0 || j > col || board[i][j] !== word[index]) return false; // 判断不符合条件
 //         if (index === word.length - 1) return true; // word遍历完了
-//         var tmp = board[i][j]; // 记录到board的值
+//         let tmp = board[i][j]; // 记录到board的值
 //         board[i][j] = '-' // 锁上，因为后续的递归是4个方向上的，无法保证上一个方向的值
-//         var res = dfs(i - 1, j, board, word, index + 1) || dfs(i + 1, j, board, word, index + 1) || dfs(i, j - 1, board, word, index + 1) || dfs(i, j + 1, board, word, index + 1);
+//         let res = dfs(i - 1, j, board, word, index + 1) || dfs(i + 1, j, board, word, index + 1) || dfs(i, j - 1, board, word, index + 1) || dfs(i, i, board, word, index + 1);
 //         board[i][j] = tmp; // 恢复现场
 //         return res;
 
 //     }
 
 //     // 遍历整个board，找到初始位置点
-//     for (var i = 0; i < row; i++) {
-//         for (var j = 0; j < col; j++) {
+//     for (let i = 0; i < row; i++) {
+//         for (let j = 0; j < col; j++) {
 //             if (dfs(i, j, board, word, 0)) return true;
 //         }
 //     }
@@ -50,4 +62,41 @@
 //     return false
 // };
 // console.log(exist(board, word));
+let arr = [0, 1, 2, 4, 8, 10, 11, 12, 13, 16, 17, 18, 18];
 
+function chaosArray (nums) {
+    nums.sort(function (a, b) {
+        return Math.random() - 0.5;
+    });
+    return nums;
+}
+chaosArray(arr);
+console.log(arr);
+function sortArray (nums) {
+    function quicksort (left, right) //传入两个哨兵
+    {
+        var key = nums[left], //定义基准数key
+            i = left, //0
+            j = right, //2
+            t;
+        if (left > right) return; //当传入哨兵错误会结束当前函数
+        while (i != j) //哨兵还未相遇继续执行循环
+        {
+            while (nums[j] >= key && i < j)
+                j--; //条件成立代表哨兵j还未找到小于基准数的值，故j--
+            while (nums[i] <= key && i < j)
+                i++; //条件成立代表哨兵i还未找到大于基准数的值，故i++
+            if (i < j) //哨兵定位后交换数值
+            {
+                nums[i] = [nums[j], nums[j] = nums[i]][0];
+            }
+        }
+        console.log(nums[left] + "  " + nums[i]);
+        nums[left] = [nums[i], nums[i] = nums[left]][0]; //此时哨兵相遇，交换基准数与哨兵的值
+        quicksort(left, i - 1);
+        quicksort(i + 1, right);
+    }
+    quicksort(0, nums.length - 1);
+    return nums;
+}
+console.log(sortArray(arr));
--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
-# Java和JavaScript回答
+# Java和JavaScript
 
 主JavaScript，副Java
 
 部分JS由于未实现LeetCode给出的数据结构，所以在本地运行不正确，在LeetCode中是正常的。
 
 除了给出自己的解答，可能还还记录了执行用时最少以及内存消耗最少的解答。
+
+## 说明
+
+[leetcode 的 JavaScript 环境](https://support.leetcode-cn.com/hc/kb/article/1194343/) 是nodejs 10.15.0，刷题建议用node，而不是浏览器。
+
+![](https://ythdong.gitee.io/blog_image/%E7%8E%A9%E8%B6%A3/facio.jpg)
